4a99708789035d5c5d6c71974377b87d733d5ba6
HishamElalawy <hisham.elalawy@gmail.com>
2021-09-24 03:30:50 +0200

    Adding users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Profile
+from projects.models import Project
 
-# Create your views here.
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles':profiles}
+    return render(request , 'users/profiles.html' , context)
+
+def userProfile(request , pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills = profile.skill_set.exclude(description='')
+    otherSkills = profile.skill_set.filter(description='')
+    projects = profile.project_set.all()
+    context = {'profile' : profile , 'topSkills' : topSkills ,'otherSkills' : otherSkills ,'projects':projects }
+    return render (request , 'users/user-profile.html' , context)
